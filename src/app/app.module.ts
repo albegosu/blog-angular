aa3c3dff75252fccaf59c378eea84dd2ab1d1cf7
Alberto <albegosu@gmail.com>
2023-07-08 22:59:04 +0200

    Create componentes: Blog, Form, Posts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { FormComponent } from './components/form/form.component';
+import { PostsComponent } from './components/posts/posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogComponent,
+    FormComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule
